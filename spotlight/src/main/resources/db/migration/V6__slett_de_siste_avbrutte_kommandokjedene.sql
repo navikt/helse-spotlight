5f5fbe065d8d6a1e5028295af3c362ed14310857
Elias Andreassen Thøgersen <elias3004@msn.com>
2024-03-13 10:44:04 +0100

    🗃️ sletter de siste avbrutte kommandokjedene
    * disse rakk å bli avbrutt rett før spotlight begynte å lese inn
    kommandokjede_avbrutt

---
+++ spotlight/src/main/resources/db/migration/V6__slett_de_siste_avbrutte_kommandokjedene.sql
@@ -0,0 +1,5 @@
+DELETE
+FROM suspenderte_kommandokjeder
+WHERE command_context_id
+          IN ('10256622-9e2c-4f13-8a1b-04f402439eca', '7950e972-5601-4f28-b852-596a7d149a95',
+              '3c98a009-771d-40bf-9f80-5ff02d2abb6c', '24046cbb-8cb6-4218-b315-e74ddbb24de0');
