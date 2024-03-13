e773a2e2c659e8a7f953974725f06436a7d4515a
Elias Andreassen Thøgersen <elias3004@msn.com>
2024-03-13 10:26:42 +0100

    🗃️ sletter flere avbrutte kommandokjeder
    * sjekket at disse var avbrutt i spesialist

---
+++ spotlight/src/main/resources/db/migration/V4__slett_flere_avbrutte_kommandokjeder.sql
@@ -0,0 +1,3 @@
+DELETE FROM suspenderte_kommandokjeder
+       WHERE command_context_id
+                 IN ('559b614e-731f-489f-8f48-b696510b553f', 'd45a301b-4c28-44cc-aad4-430d0f0d04aa', '44622e8c-0f33-4c8d-8736-beeb666b48ec');
