b0e67e1d0b908eb78bd03d7f89c0cc1cc3960739
Elias Andreassen Thøgersen <elias3004@msn.com>
2024-03-14 09:55:47 +0100

    ✅ Tester at byggSlackMelding velger riktig tittel

--- spotlight/src/test/kotlin/no/nav/helse/slack/SlackMessageBuilderTest.kt
+++ spotlight/src/test/kotlin/no/nav/helse/slack/SlackMessageBuilderTest.kt
@@ -3,15 +3,28 @@ package no.nav.helse.slack
 import no.nav.helse.Testdata.kommandokjedeSuspendertFraDatabase
 import no.nav.helse.objectMapper
 import no.nav.helse.slack.SlackMessageBuilder.byggSlackMelding
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 
 internal class SlackMessageBuilderTest {
 
     @Test
-    fun `Sjekker at byggSlackMelding lager gyldig json`() {
-        val suspenderteKommandokjeder = listOf(kommandokjedeSuspendertFraDatabase(), kommandokjedeSuspendertFraDatabase())
-        val jsonString = suspenderteKommandokjeder.byggSlackMelding(suspenderteKommandokjeder.size)
-        assertDoesNotThrow { objectMapper.readTree(jsonString) }
+    fun `Sjekker at byggSlackMelding velger riktig tittel`() {
+        val enKommandokjede = listOf(kommandokjedeSuspendertFraDatabase())
+        val enKommandokjedeJsonString = enKommandokjede.byggSlackMelding(enKommandokjede.size)
+        assertDoesNotThrow { objectMapper.readTree(enKommandokjedeJsonString) }
+        assertTrue(enKommandokjedeJsonString.contains("1 kommandokjede"))
+
+        val toKommandokjeder = listOf(kommandokjedeSuspendertFraDatabase(), kommandokjedeSuspendertFraDatabase())
+        val toKommandokjederJsonString = toKommandokjeder.byggSlackMelding(toKommandokjeder.size)
+        assertDoesNotThrow { objectMapper.readTree(toKommandokjederJsonString) }
+        assertTrue(toKommandokjederJsonString.contains("2 kommandokjeder"))
+
+        val fortsettelse = listOf(kommandokjedeSuspendertFraDatabase())
+        val fortsettelseJsonString = fortsettelse.byggSlackMelding()
+        assertDoesNotThrow { objectMapper.readTree(fortsettelseJsonString) }
+        assertTrue(fortsettelseJsonString.contains("Fortsettelse"))
     }
+
 }
