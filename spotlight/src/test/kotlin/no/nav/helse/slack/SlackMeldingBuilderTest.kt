a1739eb20a7a6dee4c660e7a0a1213ca42675ba5
Elias Andreassen Thøgersen <elias3004@msn.com>
2024-03-26 11:02:53 +0100

    ✅ Ny test på byggPåminnelseSlackMelding

--- spotlight/src/test/kotlin/no/nav/helse/slack/SlackMeldingBuilderTest.kt
+++ spotlight/src/test/kotlin/no/nav/helse/slack/SlackMeldingBuilderTest.kt
@@ -3,6 +3,7 @@ package no.nav.helse.slack
 import no.nav.helse.Testdata.kommandokjedeFraDatabase
 import no.nav.helse.objectMapper
 import no.nav.helse.slack.SlackMeldingBuilder.byggDagligSlackMelding
+import no.nav.helse.slack.SlackMeldingBuilder.byggPåminnelseSlackMelding
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
@@ -10,7 +11,7 @@ import org.junit.jupiter.api.assertDoesNotThrow
 internal class SlackMeldingBuilderTest {
 
     @Test
-    fun `Sjekker at byggSlackMelding velger riktig tittel`() {
+    fun `byggDagligSlackMelding velger riktig tittel`() {
         val enKommandokjede = listOf(kommandokjedeFraDatabase())
         val enKommandokjedeJsonString = enKommandokjede.byggDagligSlackMelding(enKommandokjede.size)
         assertDoesNotThrow { objectMapper.readTree(enKommandokjedeJsonString) }
@@ -23,6 +24,26 @@ internal class SlackMeldingBuilderTest {
 
         val fortsettelse = listOf(kommandokjedeFraDatabase())
         val fortsettelseJsonString = fortsettelse.byggDagligSlackMelding()
+        assertDoesNotThrow { objectMapper.readTree(fortsettelseJsonString) }
+        assertTrue(fortsettelseJsonString.contains("Fortsettelse"))
+    }
+
+    @Test
+    fun `byggPåminnelseSlackMelding velger riktig tittel`() {
+        val enKommandokjede = listOf(kommandokjedeFraDatabase())
+        val enKommandokjedeJsonString = enKommandokjede.byggPåminnelseSlackMelding(enKommandokjede.size)
+        assertDoesNotThrow { objectMapper.readTree(enKommandokjedeJsonString) }
+        assertTrue(enKommandokjedeJsonString.contains("1 kommandokjede"))
+        assertTrue(enKommandokjedeJsonString.contains("påminnet"))
+
+        val toKommandokjeder = listOf(kommandokjedeFraDatabase(), kommandokjedeFraDatabase())
+        val toKommandokjederJsonString = toKommandokjeder.byggPåminnelseSlackMelding(toKommandokjeder.size)
+        assertDoesNotThrow { objectMapper.readTree(toKommandokjederJsonString) }
+        assertTrue(toKommandokjederJsonString.contains("2 kommandokjeder"))
+        assertTrue(enKommandokjedeJsonString.contains("påminnet"))
+
+        val fortsettelse = listOf(kommandokjedeFraDatabase())
+        val fortsettelseJsonString = fortsettelse.byggPåminnelseSlackMelding()
         assertDoesNotThrow { objectMapper.readTree(fortsettelseJsonString) }
         assertTrue(fortsettelseJsonString.contains("Fortsettelse"))
     }
