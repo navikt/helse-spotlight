a17ca1d99f1a9b17e2164c7e5a0a9e3c42561cd4
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-04-04 18:00:20 +0200

    ✅ Test av ignorering av gamle tidspunkt-meldinger

--- src/test/kotlin/no/nav/helse/spotlight/river/HverHalvtimeRiverIntegrationTest.kt
+++ src/test/kotlin/no/nav/helse/spotlight/river/HverHalvtimeRiverIntegrationTest.kt
@@ -37,4 +37,27 @@ class HverHalvtimeRiverIntegrationTest : AbstractIntegrationTest() {
         assertEquals(kommandokjede.sisteMeldingId.toString(), påminnelseMelding["meldingId"].asText())
         assertEquals(1, testRapid.inspektør.size)
     }
+
+    @Test
+    fun `Ignorerer halvtime-meldinger som er eldre enn en halvtime`() {
+        // Given:
+        val sistePartisjonsnøkkel = "${UUID.randomUUID()}"
+        val kommandokjede =
+            lagretKommandokjede(
+                mottattTidspunkt = Instant.now().minusMinutes(31),
+                sistePartisjonsnøkkel = sistePartisjonsnøkkel,
+                antallGangerPåminnet = 0,
+            )
+        val commandContextId = kommandokjede.commandContextId
+
+        // When:
+        testRapid.sendTestMessage("""{ "@event_name": "halv_time", "klokkeslett": "${LocalTime.now().minusMinutes(31)}" }""")
+
+        // Then:
+        val lagretKommandokjede = dao.finn(commandContextId)
+        assertNotNull(lagretKommandokjede)
+        assertEquals(0, testRapid.inspektør.size)
+        assertEquals(0, lagretKommandokjede.totaltAntallGangerPåminnet)
+        assertEquals(0, lagretKommandokjede.sistSuspenderteSti.antallGangerPåminnet)
+    }
 }
